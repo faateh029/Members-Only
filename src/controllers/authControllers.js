35e8e75642d82b4c26fdb1ea9122f961c44d4f1b
faateh029 <buttfatih691@gmail.com>
2025-08-08 00:19:49 +0500

    Update signup logic

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -2,7 +2,7 @@ import {User} from '../models/userModel.js';
 import bcryptjs from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 export const signupController = async (req , res)=>{
-   const {fullname,email,password,confirmPassword} = req.body;
+   const {fullname,email,password,confirmPassword , secretKey} = req.body;
    if(password!=confirmPassword){
     return res.status(400).json({msg:"passwords do not match"});
    }
@@ -11,19 +11,39 @@ export const signupController = async (req , res)=>{
     return res.status(409).json({msg:"Email already in use"});
    }
    const hashPass= await bcryptjs.hash(password , 10);
+   
    const userData = {
     fullname ,
     email,
     password:hashPass,
+    role:"user" 
    }
-
-   const newUser= new User(userData);
+   if(secretKey===process.env.MEMBER_PASSCODE){
+    userData.role = "member";
+   }else if (secretKey===process.env.ADMIN_PASSCODE){
+    userData.role="admin";
+   }
+    const newUser= new User(userData);
     await newUser.save();
-    const accessToken = jwt.sign({user_id:_id , email , role} , process.env.JWT_SECRET , {expiresIn:"1h"});
-   res.status(200).json(  {token:accessToken 
-    ,user:{fullname:fullname,
-                         email:email,
-                        }})  
+
+    const accessToken = jwt.sign(
+    {
+     user_id:newUser._id 
+   , email:newUser.email 
+   , role:newUser.role
+    }
+    , process.env.JWT_SECRET , 
+    {
+      expiresIn:"1h"
+    });
+
+    res.status(200).json( 
+      {token:accessToken ,
+        user:{
+              fullname:fullname,
+              email:email
+             }
+      })  
    
 }
 
@@ -42,7 +62,7 @@ export const loginController = async (req , res)=>{
           user_id:validateUser._id ,
           email:validateUser.email ,
           role:validateUser.role
-          } , 
+           } , 
           process.env.JWT_SECRET  , 
           {expiresIn:"1h"}
            );
