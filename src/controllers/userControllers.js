a6b1daa12f6b5e17650dc08b66af618876e3d003
faateh029 <buttfatih691@gmail.com>
2025-08-09 23:21:28 +0500

    create winston logger

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,28 +1,40 @@
-import {User} from '../models/userModel.js';
-export const clubjoinController = async (req , res)=>{
-  const {secretKey} = req.body ;
-
-  if(!secretKey){
-    return res.status(404).json({msg:"no secret key found"})
+import jwt from 'jsonwebtoken';
+import { User } from '../models/userModel';
+export const club_join_controller = async(req ,res)=>{
+    try {
+     
+    const {secret} = req.body;
+       let token;
+       const authHeader = req.headers.authorization || req.headers.Authorization;
+        if(!authHeader){
+            return res.status(400).json({msg:"Token not found"});
+        }
+       if(authHeader && authHeader.startsWith("Bearer")){
+         token = authHeader.split(" ")[1];
+       }
+       const decodedToken = await jwt.verify(token , process.env.JWT_SECRET  )
+       req.user = decodedToken;
+       if(req.user.role==="admin"||req.user.role==="member"){
+        return res.json({msg:"No need to upgrade"})
+       }
+         if(secret===process.env.MEMBER_PASSCODE){
+            const user = User.findOne({ _id: req.user._id });
+             if(!user){
+                return res.status(400).json({msg:"user not found"})
+             }
+            user.role= "member"
+           await user.save();
+          return res.status(200).json({msg:"role updated successfully"})            
+       }
+       res.status(409).json({msg:"Access not given"})    
+    } catch (error) {
+         return res.status(500).json({msg:error.message});
     }
-  if(secretKey===process.env.CLUB_SECRET_PASSCODE){
-   
-  await User.findByIdAndUpdate(req.user._id, {role:"member"});
-  return res.status(200).json({msg:"Hi Member"});
-  }
- res.status(409).json({msg:"Access denied"});
 }
 
 
-export const allUsersController = async (req, res) => {
-    try {
-        const allUsers = await User.find(); 
-        res.status(200).json(allUsers);
-    } catch (error) {
-        res.status(500).json({ msg: "Error fetching users", error });
-    }
-};
 
-export const getDetailsController = async (req,res)=>{
-     return res.status(200).json({msg:"take details"});
+
+export const create_message_controller =async (req,res)=>{
+   
 }
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,11 +1,7 @@
 import {Router} from 'express';
 import { verifyToken } from '../middleware/jwtMiddleware.js';
 import { allowedRoles } from '../middleware/allowRole.js';
-import {clubjoinController, allUsersController , getDetailsController} from '../controllers/userControllers.js';
+import {club_join_controller , create_message_controller} from '../controllers/userControllers.js';
 export const userRouter = new Router();
-
-userRouter.post('/join-club' , verifyToken, clubjoinController);
-
-userRouter.get('/all-users' ,verifyToken,allowedRoles('admin'),allUsersController);
-
-userRouter.get('/get-details',allowedRoles('member' , 'admin') , getDetailsController);
+userRouter.post('/club-join' ,club_join_controller);
+userRouter.post('/create-message' , verifyToken,create_message_controller)
